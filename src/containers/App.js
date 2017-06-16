21ffc4559ae43f4cf152322f9314690faf3a3f95
velopert <public.velopert@gmail.com>
2017-06-17 01:16:51 +0900

    헤더와 레이아웃

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Header from 'components/Header';
+import Layout from 'components/Layout';
 
 class App extends Component {
     render() {
         return (
-            <div>
-                Hello MemoApp!
-            </div>
+            <Layout>
+                <Header/>
+                <Layout.Main>hello</Layout.Main>
+            </Layout>
         );
     }
 }
